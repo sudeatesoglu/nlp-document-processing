745d18088d5c82bd2e9eb343c30557ee429eac33
sudeatesoglu <sude.atesoglu4@gmail.com>
2024-07-11 23:13:33 +0300

    update gradio app tabs

--- scripts/app.py
+++ scripts/app.py
@@ -1,28 +1,54 @@
 import gradio as gr
-from similarity import check_doc_similarity
-from visualization import project_word_vectors
+from similarity import *
+from pca_visualization import pca_interface
 
-demo1 = gr.Interface(
+similarity_demo = gr.Interface(
     fn=check_doc_similarity,
     inputs=[
-        gr.Radio(["PDF", "TXT"], label="Document Type"),
+        gr.Radio(["PDF", "TXT", "DOCX"], label="Document Type"),
         gr.File(label="Document 1"),
         gr.File(label="Document 2"),
     ],
     outputs=gr.Textbox(label="Similarity Score"),
-    title="Document Similarity Checker",
-    description="Upload two documents to compare their similarity.",
+    title="Document Similarity Detector",
+    description="Upload two documents to check their similarity.",
 )
 
-demo2 = gr.Interface(
-    fn=project_word_vectors,
-    inputs=gr.Textbox(label="Words to Visualize"),
-    outputs=None,
-    title="Word Vector Visualization",
-    description="Enter words separated by space to visualize their word vectors.",
+categorical_similarity_demo = gr.Interface(
+    fn=check_categorical_similarity,
+    inputs=[
+        gr.Textbox(label="Category"),
+        gr.Radio(["PDF", "TXT", "DOCX"], label="Document Type"),
+        gr.File(label="Document")
+    ],
+    outputs=gr.Textbox(label="Categorical Similarity Scores"),
+    title="Categorical Similarity Detector",
+    description="Enter word to check the similarity to document."
 )
 
-demo = gr.TabbedInterface([demo1, demo2], ["Document Comparison", "Word Vector Visualization"])
+pca_demo = gr.Interface(
+    fn=pca_interface,
+    inputs=[
+        gr.Radio(["PDF", "TXT", "DOCX"], label="Document Type"),
+        gr.File(label="Document")
+    ],
+    outputs=gr.Plot(),
+    title="Document PCA Visualization",
+    description="Visualize document vectors with PCA."
+)
+
+demo = gr.TabbedInterface(
+    interface_list=[
+        similarity_demo,
+        categorical_similarity_demo,
+        pca_demo
+    ],
+    tab_names=[
+        "Document Similarity",
+        "Categorical Similarity",
+        "PCA Visualization"
+    ]
+)
 
 if __name__ == "__main__":
     demo.launch()
