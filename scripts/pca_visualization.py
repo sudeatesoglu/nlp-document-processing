7c7bae4ac1582ba46f1b472698923870a43ee4f0
sudeatesoglu <sude.atesoglu4@gmail.com>
2024-07-11 23:07:19 +0300

    update pca_visualization

--- scripts/pca_visualization.py
+++ scripts/pca_visualization.py
@@ -1,22 +1,31 @@
-import numpy as np
+import spacy
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-import spacy
+from handle_document import read_document
+from text_processing import preprocess_text
 
 nlp = spacy.load("en_core_web_md")
 
 
-def project_word_vectors(words):
-    word_ids = [nlp.vocab.strings[word] for word in words]
-    word_vectors = np.vstack([nlp.vocab.vectors[i] for i in word_ids])
+def project_document_vectors(doc_text):
+    sentences = [sent.text for sent in nlp(doc_text).sents]
+    document_vectors = [nlp(sentence).vector for sentence in sentences]
     pca = PCA(n_components=2)
-    transformed_vectors = pca.fit_transform(word_vectors)
+    transformed_vectors = pca.fit_transform(document_vectors)
 
     plt.figure(figsize=(10, 7))
     plt.scatter(transformed_vectors[:, 0], transformed_vectors[:, 1])
-    for i, word in enumerate(words):
-        plt.annotate(word, xy=(transformed_vectors[i, 0], transformed_vectors[i, 1]))
-    plt.title("Word Vectors PCA Projection")
-    plt.xlabel("PCA Component 1")
-    plt.ylabel("PCA Component 2")
+    for i, sentence in enumerate(sentences):
+        plt.annotate(f"Sentence {i + 1}", xy=(transformed_vectors[i, 0], transformed_vectors[i, 1]))
+    plt.title("Document Vectors PCA")
+    plt.grid(True)
     plt.show()
+    plt.close()
+    return plt
+
+
+def pca_interface(doc_type, doc):
+    doc_text = read_document(doc_type, doc.name)
+    doc_text = preprocess_text(doc_text)
+    plt = project_document_vectors(doc_text)
+    return plt
