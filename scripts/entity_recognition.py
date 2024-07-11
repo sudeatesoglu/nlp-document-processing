b8eb25cbf46b4b60ffdf59c5ea2c31600eda460f
sudeatesoglu <sude.atesoglu4@gmail.com>
2024-07-12 02:10:38 +0300

    add ner with diplacy

--- scripts/entity_recognition.py
+++ scripts/entity_recognition.py
@@ -1,9 +1,28 @@
 import spacy
+from spacy import displacy
+from handle_document import read_document
+from text_processing import preprocess_text
 
 nlp = spacy.load("en_core_web_md")
 
 
-def entity_recognize(document):
+def entity_recognize(doc_type, doc):
+    document = read_document(doc_type, doc)
+    document = preprocess_text(document)
     document_token = nlp(document)
-    for entity in document_token.ents:
-        return entity.text, entity.label_
+    entities = [(entity.text, entity.label_) for entity in document_token.ents]
+    return entities
+
+
+def display_entity_recognize(doc_type, doc):
+    document = read_document(doc_type, doc)
+    document = preprocess_text(document)
+    document_token = nlp(document)
+    ner = displacy.render(document_token, style="ent", jupyter=True)
+    return ner
+
+
+def ner_interface(doc_type, doc):
+    entities = entity_recognize(doc_type, doc)
+    entity_display = display_entity_recognize(doc_type, doc)
+    return entities, entity_display
