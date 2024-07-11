d89a073cc42bb0a1954eb52d97ef21830bbeb736
sudeatesoglu <sude.atesoglu4@gmail.com>
2024-07-11 23:09:02 +0300

    add file handling seperately and add docx

---
+++ scripts/handle_document.py
@@ -0,0 +1,32 @@
+import PyPDF2
+import docx
+
+
+def read_document(doc_type, file_path):
+    if doc_type == "PDF":
+        return read_pdf(file_path)
+    elif doc_type == "TXT":
+        return read_txt(file_path)
+    elif doc_type == "DOCX":
+        return read_docx(file_path)
+    else:
+        raise ValueError("Unsupported document type.")
+
+
+def read_pdf(file_path):
+    text = ""
+    with open(file_path, "rb") as file:
+        reader = PyPDF2.PdfFileReader(file)
+        for page in range(reader.numPages):
+            text += reader.getPage(page).extractText()
+    return text
+
+
+def read_txt(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def read_docx(file_path):
+    doc = docx.Document(file_path)
+    return "\n".join([paragraph.text for paragraph in doc.paragraphs])
